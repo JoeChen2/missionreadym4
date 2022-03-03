b238b6760f0ff6e68a3d67a045bcbc8c3115382e
joechen2 <blastprocessor@protonmail.com>
2022-03-04 04:01:08 +1300

    added all 5 unit tests

--- main.test.js
+++ main.test.js
@@ -5,3 +5,19 @@ console.log(remove_punc)
 test("sanity test that regular string is unmodified", function(){
     expect(remove_punc("hello world")).toBe("hello world")
 })
+
+test("testing non removed puncation", function(){
+    expect(remove_punc("hello%world")).toBe("hello%world")
+})
+
+test("testing excalmation sign removal", function(){
+    expect(remove_punc("!hello world")).toBe("hello world")
+})
+
+test("testing question mark removal", function(){
+    expect(remove_punc(";hello?world!")).toBe("helloworld")
+})
+
+test("testing question mark removal", function(){
+    expect(remove_punc("hello world?")).toBe("hello world")
+})
